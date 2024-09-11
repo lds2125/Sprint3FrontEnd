417756bc9ec9e43cce74ed9917e0170f849d528e
lds2125 <luansantos212529@gmail.com>
2024-09-11 12:27:24 -0300

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Cadastro from './components/cadastro';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Cadastro />} />
       </Routes>
