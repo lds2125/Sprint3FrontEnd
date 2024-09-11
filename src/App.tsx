ae32b717ee2edaefc48f6872958b763ea44cdbba
lds2125 <luansantos212529@gmail.com>
2024-09-11 18:25:48 -0300

    Adição de telas de login, Sobre Nós e Contato

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
-import Cadastro from './components/cadastro';
 import Navbar from './components/Navbar';
+import Cadastro from './components/cadastro';
+import SobreNos from './components/SobreNos';
+import Contato from './components/Contato';
+import JaSouClientePorto from './components/JaSouCliente';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Cadastro />} />
+        <Route path="/cadastro" element={<Cadastro />}/>
+        <Route path="/sobre-nos" element={<SobreNos />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/cliente-porto" element={<JaSouClientePorto />} />
       </Routes>
     </div>
   );
