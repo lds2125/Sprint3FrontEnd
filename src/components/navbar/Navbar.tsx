b7d3ee28d5cae7d626cc7582be31fd1c351a0642
MatheusReis48 <162203235+MatheusReis48@users.noreply.github.com>
2024-09-14 19:08:04 -0300

    NavBar responsivo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="bg-blue-600 p-4 flex justify-between items-center sticky top-0 z-50">
       <div className="flex items-center">
         <Link to="/" className="flex items-center">
           <img
-            src="/PortoFix.png" 
+            src="/PortoFix.png"
             alt="PortoFix logo"
             className="w-10 h-10"
           />
@@ -17,36 +22,63 @@ const Navbar: React.FC = () => {
           </span>
         </Link>
       </div>
-      
-      <div className="flex space-x-8 ml-auto">
-        <Link 
-          to="/sobre-nos" 
-          className="text-white hover:text-gray-300 transition-colors duration-300 ease-in-out"
-        >
-          Sobre nós
-        </Link>
-        <Link 
-          to="/contato" 
-          className="text-white hover:text-gray-300 transition-colors duration-300 ease-in-out"
-        >
-          Contato
-        </Link>
+
+      {/* Botão de menu para telas pequenas */}
+      <div className="lg:hidden">
+        <button onClick={toggleMenu} className="text-white focus:outline-none">
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d={isOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'}
+            />
+          </svg>
+        </button>
       </div>
 
-      {/* Botões à direita */}
-      <div className="flex space-x-2 ml-auto">
-        <Link
-          to="/cadastro"
-          className="bg-blue-700 text-white px-4 py-2 rounded hover:bg-white hover:text-blue-700 transition-colors duration-300 ease-in-out"
-        >
-          Cadastre-se
-        </Link>
-        <Link
-          to="/cliente-porto"
-          className="bg-white text-blue-700 px-4 py-2 rounded hover:bg-blue-700 hover:text-white transition-colors duration-300 ease-in-out"
-        >
-          Já sou cliente Porto
-        </Link>
+      {/* Links de navegação (desktop e mobile) */}
+      <div
+        className={`${
+          isOpen ? 'block' : 'hidden'
+        } w-full lg:flex lg:items-center lg:w-auto`}
+      >
+        <div className="lg:flex lg:space-x-8 mx-auto">
+          <Link
+            to="/sobre-nos"
+            className="block lg:inline-block text-white hover:text-gray-300 transition-colors duration-300 ease-in-out py-2"
+          >
+            Sobre nós
+          </Link>
+          <Link
+            to="/contato"
+            className=" ml-auto block lg:inline-block text-white hover:text-gray-300 transition-colors duration-300 ease-in-out py-2"
+          >
+            Contato
+          </Link>
+        </div>
+
+        {/* Botões à direita */}
+        <div className="lg:flex lg:space-x-2 mt-4 lg:mt-0">
+          <Link
+            to="/cadastro"
+            className="block lg:inline-block bg-blue-700 text-white px-4 py-2 rounded hover:bg-white hover:text-blue-700 transition-colors duration-300 ease-in-out"
+          >
+            Cadastre-se
+          </Link>
+          <Link
+            to="/cliente-porto"
+            className="block lg:inline-block bg-white text-blue-700 px-4 py-2 rounded hover:bg-blue-700 hover:text-white transition-colors duration-300 ease-in-out"
+          >
+            Já sou cliente Porto
+          </Link>
+        </div>
       </div>
     </nav>
   );
